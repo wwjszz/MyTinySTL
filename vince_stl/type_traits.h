ffabfe829ba6ab8d955cc025e7a1565224e1f02d
wwj <2301499594@qq.com>
2025-05-28 23:13:50 +0800

    :sparkles: feat: add is_satisfied_v and satisfies helper

--- vince_stl/type_traits.h
+++ vince_stl/type_traits.h
@@ -27,6 +27,21 @@ struct is_pair : _false_type {};
 template <class T1, class T2>
 struct is_pair<T1, T2> : _true_type {};
 
+/// is_satisfies
+
+template <template <class> class Pred, class Arg, class... Args>
+struct satisfies {
+    static constexpr bool value = Pred<Arg>::value && satisfies<Pred, Args...>::value;
+};
+
+template <template <class> class Pred, class Arg>
+struct satisfies<Pred, Arg> {
+    static constexpr bool value = Pred<Arg>::value;
+};
+
+template <template <class> class Pred, class... Args>
+inline constexpr bool is_satisfied_v = satisfies<Pred, Args...>::value;
+
 }  // namespace vince
 
 #endif
