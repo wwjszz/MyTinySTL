f0d09e61b55a6f7eea0ed7560d46d8468e971d85
wwj <2301499594@qq.com>
2025-05-29 15:37:58 +0800

    :art: feat: add constexpr to utility functions
    - Improve code structure for better maintainability
    - Fix bug in handling trivial types during copy operations
    - Add constexpr to multiple functions for compile-time evaluation support

--- vince_stl/algobase.h
+++ vince_stl/algobase.h
@@ -1,11 +1,11 @@
 #ifndef VINCE_ALGOBASE_H__
 #define VINCE_ALGOBASE_H__
 
-#include "iterator.h"
-#include "util.h"
-#include <algorithm>
-#include <cstring>
 #include <type_traits>
+
+#include "iterator.h"
+#include "type_traits.h"
+#include "util.h"
 
 #define STRINGIFY( x ) #x
 #define COMPILER_MESSAGE( msg ) _Pragma( STRINGIFY( GCC warning msg ) )
@@ -25,49 +25,46 @@ namespace vince {
 // max min
 
 template <class T>
-inline const T& max( const T& a, const T& b ) {
+inline constexpr const T& max( const T& a, const T& b ) {
     return a > b ? a : b;
 }
 
 template <class T>
-inline const T& min( const T& a, const T& b ) {
+inline constexpr const T& min( const T& a, const T& b ) {
     return a < b ? a : b;
 }
 
 template <class T, class Comp>
-inline const T& max( const T& a, const T& b, Comp comp ) {
+inline constexpr const T& max( const T& a, const T& b, Comp comp ) {
     return comp( b, a ) ? a : b;
 }
 
 template <class T, class Comp>
-inline const T& min( const T& a, const T& b, Comp comp ) {
+inline constexpr const T& min( const T& a, const T& b, Comp comp ) {
     return comp( a, b ) ? a : b;
 }
 
 // iter_swap
 
 template <class ForwardIterator1, class ForwardIterator2>
-inline void iter_swap( ForwardIterator1 a,
-                       ForwardIterator2 b ) noexcept( noexcept( swap( *std::declval<ForwardIterator1>(),
-                                                                      *std::declval<ForwardIterator2>() ) ) ) {
+inline constexpr void iter_swap( ForwardIterator1 a, ForwardIterator2 b ) noexcept(
+    noexcept( swap( *std::declval<ForwardIterator1>(), *std::declval<ForwardIterator2>() ) ) ) {
     swap( *a, *b );
 }
 
 // copy
 
-template <class InputIterator, class OutputIterator, class Distance,
-          std::enable_if_t<is_input_iteraotr_t<InputIterator>, int> = 0>
-inline OutputIterator __copy( InputIterator first, InputIterator last, OutputIterator result, input_iterator_tag,
-                              Distance* ) {
+template <class InputIterator, class OutputIterator, class Distance>
+inline constexpr OutputIterator __copy( InputIterator first, InputIterator last, OutputIterator result,
+                                        input_iterator_tag, Distance* ) {
     for ( ; first != last; ++first, ++result )
         *result = *first;
     return result;
 }
 
-template <class InputIterator, class OutputIterator, class Distance,
-          std::enable_if_t<is_input_iteraotr_t<InputIterator>, int> = 0>
-inline OutputIterator __copy( InputIterator first, InputIterator last, OutputIterator result,
-                              random_access_iterator_tag, Distance* ) {
+template <class InputIterator, class OutputIterator, class Distance>
+inline constexpr OutputIterator __copy( InputIterator first, InputIterator last, OutputIterator result,
+                                        random_access_iterator_tag, Distance* ) {
     for ( Distance n = last - first; n > 0; --n ) {
         *result = *first;
         ++first;
@@ -76,66 +73,80 @@ inline OutputIterator __copy( InputIterator first, InputIterator last, OutputIte
     return result;
 }
 
-template <class T>
-inline T* __copy( T* first, T* last, T* result ) {
-    memmove( result, first, sizeof( T ) * ( last - first ) );
+template <class Tp>
+inline Tp* __copy_trivial( Tp const* first, Tp const* last, Tp* result ) {
+    memmove( result, first, sizeof( Tp ) * ( last - first ) );
     return result + ( last - first );
 }
 
-template <class InputIterator, class OutputIterator, std::enable_if_t<is_input_iteraotr_t<InputIterator>, int> = 0>
-inline OutputIterator copy( InputIterator first, InputIterator last, OutputIterator result ) {
+template <class InputIterator, class OutputIterator,
+          std::enable_if_t<vince::is_satisfied_v<is_input_iterator, InputIterator, OutputIterator>, int> = 0>
+inline constexpr OutputIterator __copy_aux( InputIterator first, InputIterator last, OutputIterator result ) {
     return __copy( first, last, result, iterator_category( first ), distance_type( first ) );
 }
 
-template <class T>
-inline T* copy( T* first, T* last, T* result ) {
-    return __copy( first, last, result );
+template <class Tp>
+inline constexpr std::enable_if_t<std::is_trivially_copy_assignable_v<Tp>, Tp*>
+__copy_aux( Tp const* first, Tp const* last, Tp* result ) {
+    return __copy_trivial( first, last, result );
+}
+
+template <class InputIterator, class OutputIterator>
+inline constexpr OutputIterator copy( InputIterator first, InputIterator last, OutputIterator result ) {
+    return __copy_aux( first, last, result );
 }
 
 // copy_backward
 
 template <class InputIterator, class OutputIterator, class Distance,
-          std::enable_if_t<is_input_iteraotr_t<InputIterator>, int> = 0>
-inline OutputIterator __copy_backward( InputIterator first, InputIterator last, OutputIterator result,
-                                       input_iterator_tag, Distance* ) {
+          std::enable_if_t<is_input_iterator_t<InputIterator>, int> = 0>
+inline constexpr OutputIterator __copy_backward( InputIterator first, InputIterator last, OutputIterator result,
+                                                 input_iterator_tag, Distance* ) {
     while ( last != first )
         *--result = *--last;
     return result;
 }
 
 template <class InputIterator, class OutputIterator, class Distance,
-          std::enable_if_t<is_input_iteraotr_t<InputIterator>, int> = 0>
-inline OutputIterator __copy_backward( InputIterator first, InputIterator last, OutputIterator result,
-                                       random_access_iterator_tag, Distance* ) {
+          std::enable_if_t<is_input_iterator_t<InputIterator>, int> = 0>
+inline constexpr OutputIterator __copy_backward( InputIterator first, InputIterator last, OutputIterator result,
+                                                 random_access_iterator_tag, Distance* ) {
     for ( Distance n = last - first; n > 0; --n )
         *--result = *--last;
     return result;
 }
 
-template <class T>
-inline T* __copy_backward( T* first, T* last, T* result ) {
+template <class Tp>
+inline Tp* __copy_backward_trivial( Tp const* first, Tp const* last, Tp* result ) {
     const size_t n = last - first;
     result -= n;
 
-    memmove( result, first, sizeof( T ) * n );
+    memmove( result, first, sizeof( Tp ) * n );
     return result;
 }
 
-template <class InputIterator, class OutputIterator, std::enable_if_t<is_input_iteraotr_t<InputIterator>, int> = 0>
-inline OutputIterator copy_backward( InputIterator first, InputIterator last, OutputIterator result ) {
+template <class InputIterator, class OutputIterator>
+inline constexpr OutputIterator __copy_backward_aux( InputIterator first, InputIterator last, OutputIterator result ) {
     return __copy_backward( first, last, result, iterator_category( first ), distance_type( first ) );
 }
 
-template <class T>
-inline T* copy_backward( T* first, T* last, T* result ) {
-    return __copy_backward( first, last, result );
+template <class Tp>
+inline constexpr std::enable_if_t<std::is_trivially_copy_assignable_v<Tp>, Tp*>
+__copy_backward_aux( Tp const* first, Tp const* last, Tp* result ) {
+    return __copy_backward_trivial( first, last, result );
+}
+
+template <class InputIterator, class OutputIterator>
+inline constexpr OutputIterator copy_backward( InputIterator first, InputIterator last, OutputIterator result ) {
+    return __copy_backward_aux( first, last, result );
 }
 
 // copy_if
 
 template <class InputIterator, class OutputIterator, class Predicate,
-          std::enable_if_t<is_input_iteraotr_t<InputIterator>, int> = 0>
-inline OutputIterator copy_if( InputIterator first, InputIterator last, OutputIterator result, Predicate pred ) {
+          std::enable_if_t<is_input_iterator_t<InputIterator>, int> = 0>
+inline constexpr OutputIterator copy_if( InputIterator first, InputIterator last, OutputIterator result,
+                                         Predicate pred ) {
     for ( ; first != last; ++first ) {
         if ( pred( *first ) ) {
             *result = *last;
@@ -148,8 +159,8 @@ inline OutputIterator copy_if( InputIterator first, InputIterator last, OutputIt
 // copy_n
 
 template <class InputIterator, class Size, class OutputIterator,
-          std::enable_if_t<is_input_iteraotr_t<InputIterator> && !is_random_access_iterator_t<InputIterator>, int> = 0>
-inline OutputIterator copy_n( InputIterator first, Size n, OutputIterator result ) {
+          std::enable_if_t<is_input_iterator_t<InputIterator> && !is_random_access_iterator_t<InputIterator>, int> = 0>
+inline constexpr OutputIterator copy_n( InputIterator first, Size n, OutputIterator result ) {
     if ( n > 0 ) {
         *result = *first;
         ++result;
@@ -164,7 +175,7 @@ inline OutputIterator copy_n( InputIterator first, Size n, OutputIterator result
 
 template <class InputIterator, class Size, class OutputIterator,
           std::enable_if_t<is_random_access_iterator_t<InputIterator>, int> = 0>
-inline OutputIterator copy_n( InputIterator first, Size n, OutputIterator result ) {
+inline constexpr OutputIterator copy_n( InputIterator first, Size n, OutputIterator result ) {
     typedef typename iterator_traits<InputIterator>::difference_type difference_type;
     return copy( first, first + difference_type( n ), result );
 }
@@ -172,18 +183,18 @@ inline OutputIterator copy_n( InputIterator first, Size n, OutputIterator result
 // move
 
 template <class InputIterator, class OutputIterator, class Distance,
-          std::enable_if_t<is_input_iteraotr_t<InputIterator>, int> = 0>
-inline OutputIterator __move( InputIterator first, InputIterator last, OutputIterator result, input_iterator_tag,
-                              Distance* ) {
+          std::enable_if_t<is_input_iterator_t<InputIterator>, int> = 0>
+inline constexpr OutputIterator __move( InputIterator first, InputIterator last, OutputIterator result,
+                                        input_iterator_tag, Distance* ) {
     for ( ; first != last; ++first, ++result )
         *result = vince::move( *first );
     return result;
 }
 
 template <class InputIterator, class OutputIterator, class Distance,
-          std::enable_if_t<is_input_iteraotr_t<InputIterator>, int> = 0>
-inline OutputIterator __move( InputIterator first, InputIterator last, OutputIterator result,
-                              random_access_iterator_tag, Distance* ) {
+          std::enable_if_t<is_input_iterator_t<InputIterator>, int> = 0>
+inline constexpr OutputIterator __move( InputIterator first, InputIterator last, OutputIterator result,
+                                        random_access_iterator_tag, Distance* ) {
     for ( Distance n = last - first; n > 0; --n ) {
         *result = vince::move( *first );
         ++first;
@@ -192,20 +203,26 @@ inline OutputIterator __move( InputIterator first, InputIterator last, OutputIte
     return result;
 }
 
-template <class T>
-inline T* __move( T* first, T* last, T* result ) {
-    memmove( result, first, sizeof( T ) * ( last - first ) );
+template <class Tp>
+inline Tp* __move_trivial( Tp const* first, Tp const* last, Tp* result ) {
+    memmove( result, first, sizeof( Tp ) * ( last - first ) );
     return result + ( last - first );
 }
 
-template <class InputIterator, class OutputIterator, std::enable_if_t<is_input_iteraotr_t<InputIterator>, int> = 0>
-inline OutputIterator move( InputIterator first, InputIterator last, OutputIterator result ) {
+template <class InputIterator, class OutputIterator>
+inline constexpr OutputIterator __move_aux( InputIterator first, InputIterator last, OutputIterator result ) {
     return __move( first, last, result, iterator_category( first ), distance_type( first ) );
 }
 
-template <class T>
-inline T* move( T* first, T* last, T* result ) {
-    return __move( first, last, result );
+template <class Tp>
+inline constexpr std::enable_if_t<std::is_trivially_copy_assignable_v<Tp>, Tp*>
+__move_aux( Tp const* first, Tp const* last, Tp* result ) {
+    return __move_trivial( first, last, result );
+}
+
+template <class InputIterator, class OutputIterator>
+inline constexpr OutputIterator move( InputIterator first, InputIterator last, OutputIterator result ) {
+    return __move_aux( first, last, result );
 }
 
 // equal
@@ -219,7 +236,7 @@ struct equal_to {
 
 template <class InputIterator1, class InputIterator2, class Pred,
           std::enable_if_t<vince::is_satisfied_v<is_input_iterator, InputIterator1, InputIterator2>, int> = 0>
-inline bool equal( InputIterator1 first1, InputIterator1 last1, InputIterator1 first2, Pred pred ) {
+inline constexpr bool equal( InputIterator1 first1, InputIterator1 last1, InputIterator1 first2, Pred pred ) {
     for ( ; first1 != last1; ++first1, ++first2 )
         if ( !pred( first1, first2 ) )
             return false;
@@ -228,8 +245,53 @@ inline bool equal( InputIterator1 first1, InputIterator1 last1, InputIterator1 f
 
 template <class InputIterator1, class InputIterator2,
           std::enable_if_t<vince::is_satisfied_v<is_input_iterator, InputIterator1, InputIterator2>, int> = 0>
-inline bool equal( InputIterator1 first1, InputIterator1 last1, InputIterator1 first2 ) {
+inline constexpr bool equal( InputIterator1 first1, InputIterator1 last1, InputIterator1 first2 ) {
     return vince::equal( first1, last1, first2, equal_to() );
+}
+
+// move_backward
+
+template <class InputIterator, class OutputIterator, class Distance,
+          std::enable_if_t<is_input_iterator_t<InputIterator>, int> = 0>
+inline constexpr OutputIterator __move_backward( InputIterator first, InputIterator last, OutputIterator result,
+                                                 input_iterator_tag, Distance* ) {
+    while ( last != first )
+        *--result = vince::move( *--last );
+    return result;
+}
+
+template <class InputIterator, class OutputIterator, class Distance,
+          std::enable_if_t<is_input_iterator_t<InputIterator>, int> = 0>
+inline constexpr OutputIterator __move_backward( InputIterator first, InputIterator last, OutputIterator result,
+                                                 random_access_iterator_tag, Distance* ) {
+    for ( Distance n = last - first; n > 0; --n )
+        *--result = vince::move( *--last );
+    return result;
+}
+
+template <class Tp>
+inline Tp* __move_backward_trivial( Tp const* first, Tp const* last, Tp* result ) {
+    const size_t n = last - first;
+    result -= n;
+
+    memmove( result, first, sizeof( Tp ) * n );
+    return result;
+}
+
+template <class InputIterator, class OutputIterator>
+inline constexpr OutputIterator __move_backward_aux( InputIterator first, InputIterator last, OutputIterator result ) {
+    return __move_backward( first, last, result, iterator_category( first ), distance_type( first ) );
+}
+
+template <class Tp>
+inline constexpr std::enable_if_t<std::is_trivially_copy_assignable_v<Tp>, Tp*>
+__move_backward_aux( Tp const* first, Tp const* last, Tp* result ) {
+    return __move_backward_trivial( first, last, result );
+}
+
+template <class InputIterator, class OutputIterator>
+inline constexpr OutputIterator move_backward( InputIterator first, InputIterator last, OutputIterator result ) {
+    return __move_backward_aux( first, last, result );
 }
 
 }  // namespace vince
