a2e236a0c5385e82afae20f11944ee6cffd5f0ae
wwj <2301499594@qq.com>
2025-06-01 00:55:47 +0800

    :tada: feat: add initial allocator implementation

---
+++ vince_stl/allocator.h
@@ -0,0 +1,86 @@
+#ifndef VINCE_ALLOCATOR_H__
+#define VINCE_ALLOCATOR_H__
+
+#include <cstddef>
+#include <new>
+
+#include "config.h"
+#include "construct.h"
+#include "util.h"
+
+namespace vince {
+
+template <class Tp>
+class allocator {
+public:
+    typedef Tp        value_type;
+    typedef Tp*       pointer;
+    typedef const Tp* const_pointer;
+    typedef Tp&       reference;
+    typedef const Tp& const_reference;
+    typedef size_t    size_type;
+    typedef ptrdiff_t difference_type;
+
+public:
+    static Tp* allocate();
+    static Tp* allocate( size_type n );
+
+    static void deallocate( Tp* ptr ) noexcept;
+    static void deallocate( Tp* ptr, size_type n ) noexcept;
+
+    template <class... Args>
+    static void construct( Tp* ptr, Args&&... args );
+
+    static void destory( Tp* ptr ) noexcept;
+    static void destory( Tp* first, Tp* last );
+};
+
+template <class Tp>
+Tp* allocator<Tp>::allocate() {
+    if VINCE_LIKELY ( vince::requires_special_alignment<Tp>() ) {
+        return static_cast<Tp*>( ::operator new( sizeof( Tp ), static_cast<std::align_val_t>( alignof( Tp ) ) ) );
+    }
+    return static_cast<Tp*>( ::operator new( sizeof( Tp ) ) );
+}
+
+template <class Tp>
+Tp* allocator<Tp>::allocate( size_type n ) {
+    if VINCE_LIKELY ( vince::requires_special_alignment<Tp>() ) {
+        return static_cast<Tp*>( ::operator new( n * sizeof( Tp ), static_cast<std::align_val_t>( alignof( Tp ) ) ) );
+    }
+    return static_cast<Tp*>( ::operator new( n * sizeof( Tp ) ) );
+}
+
+template <class Tp>
+void allocator<Tp>::deallocate( Tp* ptr ) noexcept {
+    if VINCE_LIKELY ( vince::requires_special_alignment<Tp>() ) {
+        ::operator delete( ptr, static_cast<std::align_val_t>( alignof( Tp ) ) );
+        return;
+    }
+    ::operator delete( ptr );
+}
+
+template <class Tp>
+void allocator<Tp>::deallocate( Tp* ptr, size_type n ) noexcept {
+    allocator::deallocate( ptr );
+}
+
+template <class Tp>
+template <class... Args>
+void allocator<Tp>::construct( Tp* ptr, Args&&... args ) {
+    vince::construct_at( ptr, vince::forward<Args>( args )... );
+}
+
+template <class Tp>
+void allocator<Tp>::destory( Tp* ptr ) noexcept {
+    vince::destroy_at( ptr );
+}
+
+template <class Tp>
+void allocator<Tp>::destory( Tp* first, Tp* last ) {
+    vince::destroy( first, last );
+}
+
+};  // namespace vince
+
+#endif
