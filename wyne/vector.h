57fd6c85662dfe5147077571f936a5150e6d1343
wwj <2301499594@qq.com>
2025-06-05 21:42:02 +0800

    :zap: feat: add emplace_back...

--- wyne/vector.h
+++ wyne/vector.h
@@ -1,6 +1,7 @@
 #ifndef WYNE_VECTOR_H__
 #define WYNE_VECTOR_H__
 
+#include <cassert>
 #include <cstddef>
 #include <limits>
 #include <memory>
@@ -12,7 +13,9 @@
 #include "compressed_pair.h"
 #include "config.h"
 #include "iterator.h"
+#include "split_buffer.h"
 #include "uninitialized.h"
+#include "util.h"
 
 namespace wyne {
 
@@ -74,6 +77,17 @@ private:
         end_cap() = begin_ + n;
     }
 
+    template <class... Args>
+    constexpr void construct( pointer p, Args... args ) {
+        alloc_traits::construct( alloc(), std::to_address( p ), wyne::forward<Args>( args )... );
+    }
+
+    template <class... Args>
+    constexpr void construct_one_at_end( Args&&... args ) {
+        ConstructTransaction ct( *this, 1 );
+        construct( ct.pos++, wyne::forward<Args>( args )... );
+    }
+
     constexpr void construct_at_end( size_type n ) {
         ConstructTransaction ct( *this, n );
         const_pointer const  new_end = ct.new_end;
@@ -94,6 +108,26 @@ private:
     constexpr void construct_at_end( InputIterator first, InputIterator last, size_type n ) {
         ConstructTransaction ct( *this, n );
         ct.pos = wyne::uninitialized_allocator_copy( alloc(), first, last, ct.pos );
+    }
+
+    inline constexpr size_type recommend( size_type new_size ) const {
+        const size_type ms = max_size();
+        if ( new_size > ms )
+            throw_length_error();
+        const size_type cap = capacity();
+        if ( cap >= ( ms >> 1 ) )
+            return ms;
+        return wyne::max<size_type>( cap << 1, new_size );
+    }
+
+    constexpr void swap_out_circular_buffer( split_buffer<value_type, allocator_type&>& v_ ) {
+        wyne::uninitialized_allocator_relocate( alloc(), std::to_address( begin_ ), std::to_address( end_ ),
+                                                std::to_address( v_.first_ ) );
+        end_      = begin_;
+        v_.begin_ = v_.first_;
+        wyne::swap( this->begin_, v_.begin_ );
+        wyne::swap( this->end_, v_.end_ );
+        wyne::swap( this->end_cap(), v_.end_cap() );
     }
 
 public:
@@ -122,6 +156,37 @@ public:
 
     constexpr size_type max_size() const noexcept {
         return wyne::min<size_type>( alloc_traits::max_size( alloc() ), std::numeric_limits<difference_type>::max() );
+    }
+
+    constexpr size_type capacity() const noexcept { return static_cast<size_type>( end_cap() - begin_ ); }
+
+    constexpr size_type size() const noexcept { return static_cast<size_type>( end_ - begin_ ); }
+
+    constexpr bool empty() const noexcept { return end_ == begin_; }
+
+    template <class... Args>
+    inline constexpr pointer emplace_back_slow_path( Args&&... args ) {
+        const size_type                           sz = size();
+        split_buffer<value_type, allocator_type&> v_( recommend( sz + 1 ), sz, alloc() );
+        construct( v_.begin_, wyne::forward<Args>( args )... );
+        ++v_.end_;
+        swap_out_circular_buffer( v_ );
+        return this->end_;
+    }
+
+    template <class... Args>
+    inline constexpr reference emplace_back( Args&&... args ) {
+        auto end_ = this->end_;
+        if ( end_ < this->end_cap() ) {
+            construct_one_at_end( wyne::forward<Args>( args )... );
+            ++end_;
+        }
+        else {
+            end_ = emplace_back_slow_path( wyne::forward<Args>( args )... );
+        }
+        // assert( this->end_ == end_ );
+        // this->end_ = end_;
+        return *( end_ - 1 );
     }
 };
 
