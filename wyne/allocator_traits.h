6f16d5c47abf24f98704a29cdf96113803a08756
wwj <2301499594@qq.com>
2025-06-05 21:38:49 +0800

    :bug: Fix: improve implementation of is_trivially_relocate

--- wyne/allocator_traits.h
+++ wyne/allocator_traits.h
@@ -2,20 +2,23 @@
 #define WYNE_ALLOCATOR_TRAITS_H__
 
 #include "type_traits.h"
+#include <type_traits>
 
 namespace wyne {
 
 // is_trivially_relocate
 
-template <class Tp>
-struct is_trivially_relocate : public first_true<
 #if __has_builtin( __is_trivially_relocatable )
-                                   _bool_constant<__is_trivially_relocatable( Tp )>,
+template <class _Tp, class = void>
+struct is_trivially_relocate : _bool_constant<__is_trivially_relocatable( _Tp )> {};
 #else
-                                   std::is_trivially_copyable<Tp>,
+template <class _Tp, class = void>
+struct is_trivially_relocate : is_trivially_copyable<_Tp> {};
 #endif
-                                   _false_type> {
-};
+
+template <class _Tp>
+struct is_trivially_relocate<_Tp, std::enable_if_t<std::is_same_v<_Tp, typename _Tp::__trivially_relocatable>>>
+    : _true_type {};
 
 }  // namespace wyne
 
